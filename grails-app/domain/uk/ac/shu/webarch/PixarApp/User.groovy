7d04c05f5decd298f53a48c41aecf724d208ac29
student <student@debian>
2014-05-09 19:26:32 +0100

    Adding basic fields into User and Film classes

--- grails-app/domain/uk/ac/shu/webarch/PixarApp/User.groovy
+++ grails-app/domain/uk/ac/shu/webarch/PixarApp/User.groovy
@@ -2,6 +2,16 @@ package uk.ac.shu.webarch.PixarApp
 
 class User {
 
+	String username
+	String userpassword
+	String userEmail
+	Integer age
+	String userBio
+	String favouriteFilm
+
     static constraints = {
+	username blank: false, nullable: false
+        userpassword blank: false, nullable: false
+        userEmail blank: false, nullable: false
     }
 }

90fd5cb89d295768064f256ab37038450915f700
student <student@debian>
2014-05-11 02:20:47 +0100

    second backup of the app, from the completion of all domain classes with relevant code to add fields, constraints, mapping and joins to the tables. Controllers to be started next

---
+++ test/unit/uk/ac/shu/webarch/PixarApp/FilmActorSpec.groovy
@@ -0,0 +1,20 @@
+package uk.ac.shu.webarch.PixarApp
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(FilmActor)
+class FilmActorSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
